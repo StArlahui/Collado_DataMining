06faaa78cc2b26b3fdb031175ced17a29a00d9d8
unknown <talacollado55@gmail.com>
2025-09-15 20:49:36 +0800

    Added Assignments 2 to 5 with code and screenshots

---
+++ Assignment_2/Assignment2.R
@@ -0,0 +1,12 @@
+marks <- 72
+if (marks >= 90 && marks <= 100){
+  print("Grade: A")
+} else if (marks >= 80 && marks <= 89){
+  print("Grade: B")
+} else if (marks >= 70 && marks <= 79){
+  print("Grade: C")
+} else if (marks >= 60 && marks <= 69){
+  print("Grade: D")
+} else{
+  print("Grade: F")
+}
