4bf22f94dc3a33dd948720c0f0794e8a9afc27b3
StArlahui <talacollado55@gmail.com>
2025-12-03 22:44:07 +0800

    Update Asssignment_1_FINALS.R

--- Asssignment_1_FINALS/Asssignment_1_FINALS.R
+++ Asssignment_1_FINALS/Asssignment_1_FINALS.R
@@ -1,21 +1,21 @@
 library(arules)
 
 
-data("Groceries")
+data("Groceries") # load dataset
 
-summary(Groceries)
-inspect(head(Groceries, 5)) 
+summary(Groceries) # check dataset summary
+inspect(head(Groceries, 5)) # show first few transactions
 
 frequent_items <- apriori(Groceries, parameter = list(supp = 0.01, target = "frequent itemsets"))
-inspect(head(frequent_items))
+inspect(head(frequent_items))    # show some frequent items
 
 rules_groceries <- apriori(Groceries, 
-                           parameter = list(supp = 0.01, conf = 0.5))
+                           parameter = list(supp = 0.01, conf = 0.5))  # generate rules
 
-rules_lift_groceries <- sort(rules_groceries, by = "lift", decreasing = TRUE)
+rules_lift_groceries <- sort(rules_groceries, by = "lift", decreasing = TRUE)   # sort rules
 
 print("Top 10 Rules for Groceries (Sorted by Lift):")
-inspect(head(rules_lift_groceries, 10))
+inspect(head(rules_lift_groceries, 10))   # show top 10 rules
 
 library(arulesViz)
 plot(head(rules_lift_synthetic, 20), method = "graph")
